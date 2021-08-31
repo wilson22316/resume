ece1c35357d103f51e0f2fb4e3f9eeefcc034df0
wilson <wilson21109@gmail.com>
2021-08-31 16:42:46 +0800

    ver1.0

---
+++ index.js
@@ -0,0 +1,22 @@
+jQuery(document).ready(function(){
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+        
+        if (scroll >= 1) {
+            $(".header").addClass("header--active");
+            $('.nav_link').addClass("nav_link--active");
+        }else{
+            $(".header").removeClass("header--active");
+            $(".nav_link").removeClass("nav_link--active");
+        
+        }
+    });
+
+    $('.burgerbtn').click(function(){
+        $('#Burger').toggleClass('crossline')
+    })
+
+    $('.cover_content').hover(function(){
+        $('.cover_content').toggleClass('cover_show')
+    })
+});
