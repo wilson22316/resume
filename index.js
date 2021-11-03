13b84f29a0fe5801b5929f8cf450405a8d3e2248
黃貴廷 <wilsonhuang@huangguitingdeMacBook-Air.local>
2021-11-03 16:19:41 +0800

    ver 2.0
    加入小林眼鏡、微調內容

--- index.js
+++ index.js
@@ -1,22 +1,26 @@
-jQuery(document).ready(function(){
-    $(window).scroll(function() {    
+jQuery(document).ready(function () {
+    $(window).scroll(function () {
         var scroll = $(window).scrollTop();
-        
+
         if (scroll >= 1) {
             $(".header").addClass("header--active");
             $('.nav_link').addClass("nav_link--active");
-        }else{
+        } else {
             $(".header").removeClass("header--active");
             $(".nav_link").removeClass("nav_link--active");
-        
+
         }
     });
 
-    $('.burgerbtn').click(function(){
+    $('.burgerbtn').click(function () {
         $('#Burger').toggleClass('crossline')
     })
 
-    $('.cover_content').hover(function(){
+    $('.cover_content').hover(function () {
         $('.cover_content').toggleClass('cover_show')
+    })
+
+    $('.cover_content2').hover(function () {
+        $('.cover_content2').toggleClass('cover_show')
     })
 });
